85ac53e95cbd7e809298cf8b0569283b9af713af
jerem509 <jcharles3@byupathway.edu>
2025-08-15 22:56:06 -0400

    modal added on each product

--- finalproject/scripts/getData.js
+++ finalproject/scripts/getData.js
@@ -1,6 +1,7 @@
-
+const myModal = document.querySelector('#my-modal');
 const gallery = document.querySelector('#products-gallery');
 const rProd = document.querySelector("#moving-banner");
+
 
 export async function fetchData(aLink, fn, condition='') {
     try {
@@ -18,19 +19,38 @@ export async function fetchData(aLink, fn, condition='') {
 }
 
 export function populateGallery(product) {
-    // ------------------ populate gallery cards for eah product -------------------
-    //create a card for each product in the array of products
-    const card = document.createElement("div");
-    card.classList.add("card");
-    //populate each card
-    card.innerHTML = ` <div class="img-container"><img src="${product.url_image}" alt="${product.name}" loading="lazy"></div>
+  // ------------------ populate gallery cards for eah product -------------------
+  //create a card for each product in the array of products
+  const card = document.createElement("div");
+  card.classList.add("card");
+  //populate each card
+  card.innerHTML = ` <div class="img-container"><img src="${product.url_image}" alt="${product.name}" loading="lazy"></div>
+                      <div class="pd-info-box">
+                          <div class="prod-info pd-name"><strong>Product Name : </strong><span class="info-to-fill">${product.name}</span></div>
+                          <div class="prod-info"><strong>Price : </strong>$ <span class="info-to-fill">${product.price}</span></div>
+                          <div class="prod-info"><strong>Available : </strong><span class="info-to-fill">${product.available}</span></div>
+                      </div>`;
+  //append the card to the gallery
+  gallery.appendChild(card);
+  card.addEventListener('click', () => {
+    myModal.innerHTML = ` <div class="img-container"><img src="${product.url_image}" alt="${product.name}" loading="lazy"></div>
+                        <button id="close-btn">
+                          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#EA3323">
+                              <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />  
+                          </svg>
+                        </button>
                         <div class="pd-info-box">
                             <div class="prod-info pd-name"><strong>Product Name : </strong><span class="info-to-fill">${product.name}</span></div>
-                            <div class="prod-info"><strong>Price : </strong>$ <span class="info-to-fill">${product.price}</span></div>
-                            <div class="prod-info"><strong>Available : </strong><span class="info-to-fill">${product.available}</span></div>
+                            <div class="prod-info"><strong>Description : </strong><span class="info-to-fill">${product.description}</span></div>
                         </div>`;
-    //append the card to the gallery
-    gallery.appendChild(card);
+    myModal.showModal();
+
+    const closeBtn = document.querySelector("#my-modal #close-btn");
+    closeBtn.addEventListener("click", () => {
+      myModal.close();
+    });
+  });
+
 }
 
 export function populateBanner(product) {
